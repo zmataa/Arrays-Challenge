08e8cc82f7fd38b74bd1814cc49d2195df52c588
Tom Bredemeier <tbredemeier@barrington220.org>
2021-10-11 10:31:54 -0500

    Initial Commit

---
+++ Arrays Challenge/Arrays_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Arrays_ChallengeApp.swift
+//  Arrays Challenge
+//
+//  Created by Tom Bredemeier on 10/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Arrays_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
